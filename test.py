d18af245a801145a01b2252be672e8ca0d84cf9a
Akash <akash.darknight@gmail.com>
2017-07-20 10:03:00 +0530

    Python Basics

--- test.py
+++ test.py
@@ -378,3 +378,18 @@ def L1(yhat, y):
 
 
 # In[42]:
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L1 = " + str(L1(yhat, y)))
+
+
+# **Expected Output**:
+#
+# <table style="width:20%">
+#
+#      <tr>
+#        <td> **L1** </td>
+#        <td> 1.1 </td>
+#      </tr>
+# </table>
