6ba9750309dfdff7101d7ec8328e26cf1737c32e
Akash <akash.darknight@gmail.com>
2017-07-21 10:03:00 +0530

    Python Basics

--- test.py
+++ test.py
@@ -393,3 +393,26 @@ print("L1 = " + str(L1(yhat, y)))
 #        <td> 1.1 </td>
 #      </tr>
 # </table>
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    loss = np.dot(np.abs(yhat - y), np.abs(yhat - y))
+    ### END CODE HERE ###
+
+    return loss
+
+
+# In[46]:
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L2 = " + str(L2(yhat, y)))
